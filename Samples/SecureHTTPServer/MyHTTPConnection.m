f25a40bd6104912b75b66eb36826808620871c79
kenial <keniallee@gmail.com>
2013-06-04 23:27:16 -0700

    Adding .p12 file support to SecureHTTPServer sample.

--- Samples/SecureHTTPServer/MyHTTPConnection.m
+++ Samples/SecureHTTPServer/MyHTTPConnection.m
@@ -30,13 +30,39 @@ static const int httpLogLevel = HTTP_LOG_LEVEL_WARN; // | HTTP_LOG_FLAG_TRACE;
 {
 	HTTPLogTrace();
 	
-	NSArray *result = [DDKeychain SSLIdentityAndCertificates];
+    // Create SSL identity & cert in dynamic manner
+    NSArray *result = [DDKeychain SSLIdentityAndCertificates];
 	if([result count] == 0)
 	{
 		[DDKeychain createNewIdentity];
 		return [DDKeychain SSLIdentityAndCertificates];
 	}
 	return result;
+    
+    // Or, you can import existing server.p12 file.
+    // Refers to http://stackoverflow.com/questions/12513291/ios-mutual-authentication
+    SecIdentityRef identityApp = nil;
+    NSString *bundlePath = [[NSBundle mainBundle] resourcePath];
+    NSString *myFilePath = [bundlePath stringByAppendingPathComponent:@"server.p12"];
+    NSData *PKCS12Data = [NSData dataWithContentsOfFile:myFilePath];
+    
+    CFDataRef inPKCS12Data = (__bridge CFDataRef)PKCS12Data;
+    CFStringRef password = CFSTR("password");
+    const void *keys[] = { kSecImportExportPassphrase };
+    const void *values[] = { password };
+    CFDictionaryRef options = CFDictionaryCreate(NULL, keys, values, 1, NULL, NULL);
+    CFArrayRef items = CFArrayCreate(NULL, 0, 0, NULL);
+    OSStatus securityError = SecPKCS12Import(inPKCS12Data, options, &items);
+    CFRelease(options);
+    CFRelease(password);
+    if (securityError == errSecSuccess) {
+        HTTPLogVerbose(@"Success opening p12 certificate. Items: %ld", CFArrayGetCount(items));
+        CFDictionaryRef identityDict = CFArrayGetValueAtIndex(items, 0);
+        identityApp = (SecIdentityRef)CFDictionaryGetValue(identityDict, kSecImportItemIdentity);
+    } else {
+        HTTPLogInfo(@"Error opening Certificate.");
+    }
+    return [NSArray arrayWithObject:(__bridge id)identityApp];
 }
 
 @end
