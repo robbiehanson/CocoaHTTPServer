eeac4e5a987cdcc8b352633893c6346777417dad
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-09-13 03:33:43 -0400

    updated umbrella header and removed CocoaLumberjack as dependency

--- Framework/CocoaHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
+++ Framework/CocoaHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
@@ -15,4 +15,12 @@ FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
 FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
 
 #import <CocoaHTTPServer/HTTPServer.h>
+#import <CocoaHTTPServer/HTTPConnection.h>
+#import <CocoaHTTPServer/HTTPLogging.h>
+#import <CocoaHTTPServer/HTTPAuthenticationRequest.h>
+#import <CocoaHTTPServer/HTTPMessage.h>
+#import <CocoaHTTPServer/HTTPResponse.h>
+#import <CocoaHTTPServer/WebSocket.h>
+#import <CocoaHTTPServer/HTTPServer.h>
+
 
