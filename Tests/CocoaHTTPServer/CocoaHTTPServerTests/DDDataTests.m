da44c8e239eab33e474c26d301e4a2699b86fd3d
Antonio Tanzola <atanzola@expedia.com>
2015-04-18 15:34:30 +0100

    Cleanup DDDataTests

--- Tests/CocoaHTTPServer/CocoaHTTPServerTests/DDDataTests.m
+++ Tests/CocoaHTTPServer/CocoaHTTPServerTests/DDDataTests.m
@@ -7,18 +7,6 @@
 
 @implementation CocoaHTTPServerTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testMD5Digest
 {
     NSString *testString = @"I love testing this stuff";
