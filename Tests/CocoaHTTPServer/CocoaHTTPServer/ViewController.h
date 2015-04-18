867d921714ce40a05eff4ddd5bb5bf44620796c7
Antonio Tanzola <atanzola@expedia.com>
2015-04-18 13:52:27 +0100

    Initial commit - created a new project to create a run tests.

---
+++ Tests/CocoaHTTPServer/CocoaHTTPServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaHTTPServer
+//
+//  Created by Antonio Tanzola on 18/04/2015.
+//  Copyright (c) 2015 Antonio Tanzola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tests/CocoaHTTPServer/CocoaHTTPServer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CocoaHTTPServer
+//
+//  Created by Antonio Tanzola on 18/04/2015.
+//  Copyright (c) 2015 Antonio Tanzola. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
