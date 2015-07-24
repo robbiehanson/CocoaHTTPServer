d8594f74aea62784f8125b833e1d1f0f8c3a73e9
Tom Meier <tom@venombytes.com>
2015-07-24 16:18:10 +0200

    Update to 2.x Cocoalumberjack usage

--- Core/HTTPLogging.h
+++ Core/HTTPLogging.h
@@ -1,52 +1,52 @@
 /**
  * In order to provide fast and flexible logging, this project uses Cocoa Lumberjack.
- * 
+ *
  * The Google Code page has a wealth of documentation if you have any questions.
  * https://github.com/robbiehanson/CocoaLumberjack
- * 
+ *
  * Here's what you need to know concerning how logging is setup for CocoaHTTPServer:
- * 
+ *
  * There are 4 log levels:
  * - Error
  * - Warning
  * - Info
  * - Verbose
- * 
+ *
  * In addition to this, there is a Trace flag that can be enabled.
  * When tracing is enabled, it spits out the methods that are being called.
- * 
+ *
  * Please note that tracing is separate from the log levels.
  * For example, one could set the log level to warning, and enable tracing.
- * 
+ *
  * All logging is asynchronous, except errors.
  * To use logging within your own custom files, follow the steps below.
- * 
+ *
  * Step 1:
  * Import this header in your implementation file:
- * 
+ *
  * #import "HTTPLogging.h"
- * 
+ *
  * Step 2:
  * Define your logging level in your implementation file:
- * 
+ *
  * // Log levels: off, error, warn, info, verbose
  * static const int httpLogLevel = HTTP_LOG_LEVEL_VERBOSE;
- * 
+ *
  * If you wish to enable tracing, you could do something like this:
- * 
+ *
  * // Debug levels: off, error, warn, info, verbose
  * static const int httpLogLevel = HTTP_LOG_LEVEL_INFO | HTTP_LOG_FLAG_TRACE;
- * 
+ *
  * Step 3:
  * Replace your NSLog statements with HTTPLog statements according to the severity of the message.
- * 
+ *
  * NSLog(@"Fatal error, no dohickey found!"); -> HTTPLogError(@"Fatal error, no dohickey found!");
- * 
+ *
  * HTTPLog works exactly the same as NSLog.
  * This means you can pass it multiple variables just like NSLog.
 **/
 
-#import "DDLog.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 // Define logging context for every log message coming from the HTTP server.
 // The logging context can be extracted from the DDLogMessage from within the logging framework,
@@ -69,7 +69,7 @@
 
 // Setup fine grained logging.
 // The first 4 bits are being used by the standard log levels (0 - 3)
-// 
+//
 // We're going to add tracing, but NOT as a log level.
 // Tracing can be turned on and off independently of log level.
 
