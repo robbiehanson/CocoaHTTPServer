69e42bfb7b7bf9259a0874cf155400734f0438c5
Luke Griffioen <l.griffioen@techsmith.com>
2015-08-20 14:32:18 -0400

    reduced console spam

--- Core/HTTPServer.m
+++ Core/HTTPServer.m
@@ -10,7 +10,7 @@
 
 // Log levels: off, error, warn, info, verbose
 // Other flags: trace
-static const int httpLogLevel = HTTP_LOG_LEVEL_INFO; // | HTTP_LOG_FLAG_TRACE;
+static const int httpLogLevel = HTTP_LOG_LEVEL_ERROR; // HTTP_LOG_LEVEL_INFO; // | HTTP_LOG_FLAG_TRACE;
 
 @interface HTTPServer (PrivateAPI)
 
