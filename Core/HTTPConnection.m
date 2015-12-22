b6ce58734e8440aa89b86c7ed15c330da0b3f69d
Zeev Glozman <z@beame.io>
2015-12-22 23:03:49 +0200

    Update HTTPConnection.m

--- Core/HTTPConnection.m
+++ Core/HTTPConnection.m
@@ -621,8 +621,8 @@ static NSMutableArray *recentNonces;
 						 forKey:(NSString *)kCFStreamSSLCertificates];
 			
 			// Configure this connection to use the highest possible SSL level
-			[settings setObject:(NSString *)kCFStreamSocketSecurityLevelNegotiatedSSL
-						 forKey:(NSString *)kCFStreamSSLLevel];
+//			[settings setObject:(NSString *)kCFStreamSocketSecurityLevelNegotiatedSSL
+//						 forKey:(NSString *)kCFStreamSSLLevel];
 			
 			[asyncSocket startTLS:settings];
 		}
