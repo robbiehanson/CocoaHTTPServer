bff051f27d6ae2a2ea85992644a8d334f85353d5
Rob Allie <r.allie@techsmith.com>
2020-05-18 13:04:04 -0400

    create project with framework in it

---
+++ CocoaHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,19 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by Allie, Robert on 5/18/20.
+//  Copyright © 2020 TechSmith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for CocoaHTTPServer.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServer/PublicHeader.h>
+
+
