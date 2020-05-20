3e4a726fc9e47e1ec3cea026c6c0f7f6773b7f96
Luke Griffioen <l.griffioen@techsmith.com>
2020-05-20 09:01:00 -0400

    fix another warning

--- Core/Mime/MultipartFormDataParser.m
+++ Core/Mime/MultipartFormDataParser.m
@@ -246,7 +246,7 @@
 			NSUInteger sizeToPass = workingData.length - offset - sizeToLeavePending;
 
 			// if we parse BASE64 encoded data, or Quoted-Printable data, we will make sure we don't break the format
-			int leaveTrailing = [self numberOfBytesToLeavePendingWithData:data length:sizeToPass encoding:currentEncoding];
+			int leaveTrailing = [self numberOfBytesToLeavePendingWithData:data length:(int)sizeToPass encoding:currentEncoding];
 			sizeToPass -= leaveTrailing;
 			
 			if( sizeToPass <= 0 ) {
