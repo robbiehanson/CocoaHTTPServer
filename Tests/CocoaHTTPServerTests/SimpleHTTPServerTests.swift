0aa39ad33ed116a5a5fa3bb0ef940ad494446dae
Christian Beer <christian.beer@chbeer.de>
2022-08-29 17:07:09 +0200

    More fixes

---
+++ Tests/CocoaHTTPServerTests/SimpleHTTPServerTests.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleHTTPServerTests.swift
+//  
+//
+//  Created by Christian Beer on 29.08.22.
+//
+
+import XCTest
+@testable import CocoaHTTPServer
+
+class SimpleHTTPServerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let server = HTTPServer()
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
